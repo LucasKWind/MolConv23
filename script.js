b25da26130014abd4c5c30b8a2eeb6cdfa54d97a
LucasKWind <129434206+LucasKWind@users.noreply.github.com>
2023-03-31 07:18:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -14,11 +14,15 @@ MolSubstance.addEventListener("keyup",function(e){
 clearTimeout(timer);
 timer = setTimeout(() => {
 inputvalue = Number(MolSubstance.innerHTML);
+System.out.println(inputvalue);
 VolVal = inputvalue*22.4;
+System.out.println(VolVal);
 VolSubstance.innerHTML = VolVal;
 MassVal = inputvalue*55.85;
+System.out.println(MassVal);
 MassSubstance.innerhtml = MassVal;
 ParVal = inputvalue*6.02e23;
+System.out.println(ParVal);
 PartSubstance.innerhtml = ParVal;
  alert('Converted values!');
 },1000);
