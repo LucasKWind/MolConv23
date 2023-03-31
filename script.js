d32ed2ced0608eecf3e00b300c240b0de6d327d8
LucasKWind <129434206+LucasKWind@users.noreply.github.com>
2023-03-30 22:57:45 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 document.getElementById('MolSubstance').style.textalign="center";
+const MolSubstance = document.querySelector('#MolSubstance');
+const VolSubstance = document.querySelector('#VolSubstance');
+const MassSubstance = document.querySelector('MassSubstance');
+let subvalue = "";
+let inputvalue = 0;
+let VolVal = 0;
+let MassVal = 0;
+let MolVal = 0;
+let timer;
+
+MolSubstance.addEventListener("keyup",function(e){
+clearTimeout(timer);
+timer = setTimeout(() => {
+inputvalue = +MolSubstance.textcontent.substr(1,4);
+subvalue = MolSubstance.textcontent.substr(5,20);
+VolVal = inputvalue*22.4
+VolSubstance.textcontent = VolVal
+
+},1000);
+});
