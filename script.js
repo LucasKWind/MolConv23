09c67073908ed24a665245f9f6d701cad83f7733
LucasKWind <129434206+LucasKWind@users.noreply.github.com>
2023-03-31 08:01:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,58 @@ PartSubstance.value = ParVal;
  alert('Converted values!');
 },1000);
 });
+
+VolSubstance.addEventListener("keyup",function(e){
+clearTimeout(timer);
+timer = setTimeout(() => {
+inputvalue = Number(VolSubstance.value);
+
+MolVal = inputvalue/22.4;
+
+MolSubstance.value = MolVal;
+MassVal = MolVal*55.85;
+
+MassSubstance.value = MassVal;
+ParVal = MolVal*6.02e23;
+
+PartSubstance.value = ParVal;
+ alert('Converted values!');
+},1000);
+});
+
+
+MassSubstance.addEventListener("keyup",function(e){
+clearTimeout(timer);
+timer = setTimeout(() => {
+inputvalue = Number(MassSubstance.value);
+
+MolVal = inputvalue/55.85;
+
+MolSubstance.value = MolVal;
+VolVal = MolVal*22.4;
+
+VolSubstance.value = VolVal;
+ParVal = MolVal*6.02e23;
+
+PartSubstance.value = ParVal;
+ alert('Converted values!');
+},1000);
+});
+
+PartSubstance.addEventListener("keyup",function(e){
+clearTimeout(timer);
+timer = setTimeout(() => {
+inputvalue = Number(PartSubstance.value);
+
+MolVal = inputvalue/6.02e23;
+
+MolSubstance.value = MolVal;
+VolVal = MolVal*22.4;
+
+VolSubstance.value = VolVal;
+MassVal = MolVal*55.85;
+
+MassSubstance.value = MassVal;
+ alert('Converted values!');
+},1000);
+});
